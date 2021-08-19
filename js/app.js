59d64a366dd35e403429a077c78c222ecf805603
brianjtarte <brianjtarte@gmail.com>
2021-08-18 19:43:23 -0700

    lab 12 chart

--- js/app.js
+++ js/app.js
@@ -4,15 +4,15 @@
 let allProducts = [];
 
 let myContainer = document.querySelector('section');
-let myButton = document.querySelector('section + div');
+
 let image1 = document.querySelector('section img:first-child');
 let image2 = document.querySelector('section img:nth-child(2)');
 let image3 = document.querySelector('section img:nth-child(3)');
-
+let indexArray = [];
 let clicks = 0;
 let clicksAllowed = 25;
 
-function Products(name, fileExtension = 'jpg') {
+function Products( name, fileExtension = 'jpg') {
   this.name = name;
   this.src = `img/${name}.${fileExtension}`;
   this.views = 0;
@@ -23,16 +23,18 @@ function selectRandomProduct() {
   return Math.floor(Math.random() * allProducts.length);
 }
 function renderProducts() {
- 
-  let prod1 = selectRandomProduct();
-  let prod2 = selectRandomProduct();
-  let prod3 = selectRandomProduct();
-
-  
-  while  (prod1 === prod2 || prod1 === prod3 || prod2 === prod3) {
-    prod2 = selectRandomProduct();
-    prod3 = selectRandomProduct();
+  while (indexArray.length < 6) {
+    let randomNumber = selectRandomProduct();
+    if (!indexArray.includes(randomNumber)){
+      indexArray.push(randomNumber);
+    }
   }
+  console.log(indexArray);
+  let prod1 = indexArray.shift();
+  let prod2 = indexArray.shift();
+  let prod3 = indexArray.shift();
+
+
   image1.src = allProducts[prod1].src;
   image2.src = allProducts[prod2].src;
   image3.src = allProducts[prod3].src;
@@ -42,6 +44,7 @@ function renderProducts() {
   allProducts[prod1].views++;
   allProducts[prod2].views++;
   allProducts[prod3].views++;
+  console.log(indexArray);
 }
 function handleProductClick(event) {
   if (event.target === myContainer) {
@@ -58,18 +61,11 @@ function handleProductClick(event) {
   }
   renderProducts();
   if (clicks === clicksAllowed) {
-    myButton.className = 'clicks-allowed';
     myContainer.removeEventListener('click', handleProductClick);
+    renderChart();
   }
 }
-function renderResults() {
-  let ul = document.querySelector('ul');
-  for (let i = 0; i < allProducts.length; i++) {
-    let li = document.createElement('li');
-    li.textContent = `${allProducts[i].name} had ${allProducts[i].views} views and was select ${allProducts[i].clicks} times.`;
-    ul.appendChild(li);
-  }
-}
+
 new Products ('bag');
 new Products ('banana');
 new Products ('bathroom');
@@ -91,9 +87,49 @@ new Products ('water-can');
 new Products ('wine-glass');
 
 
-console.log(allProducts);
 renderProducts();
 
+function renderChart() {
+  let productClicks = [];
+  let productViews = [];
+  let productNames = [];
+  for (let i = 0; i < allProducts.length; i++) {
+    productNames.push(allProducts[i].name);
+    productClicks.push(allProducts[i].clicks);
+    productViews.push(allProducts[i].views);
+  }
+  
+  let chartObject = {
+    type: 'bar',
+    data: {
+      labels: productNames,
+      datasets: [{
+        label: '# of Views',
+        data: productViews,
+        backgroundColor: 'green',
+        borderColor: 'green',
+        borderWidth: 1
+      },
+      {
+        label: '# of Clicks',
+        data: productClicks,
+        backgroundColor: 'violet',
+        borderColor: 'violet',
+        borderWidth: 1
+        
+      }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  };
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, chartObject);
+}
 
 myContainer.addEventListener('click', handleProductClick);
-myButton.addEventListener('click', renderResults);
